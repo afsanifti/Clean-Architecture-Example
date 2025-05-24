0d3530d8bcb9a6271236adbe8c2c4d1b9b4e7966
afsanifti <afsanisifti@gmail.com>
2025-05-24 15:03:08 +0600

    REmote Data sourece implementation and test update

--- lib/src/authentication/data/datasource/authentication_remote_data_source.dart
+++ lib/src/authentication/data/datasource/authentication_remote_data_source.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:testingapp/core/errors/exceptions.dart';
+import 'package:testingapp/core/utils/typedefs.dart';
 import 'package:testingapp/src/authentication/data/models/user_models.dart';
 import 'package:http/http.dart' as http;
 
@@ -15,8 +17,8 @@ abstract class AuthenticationRemoteDataSource {
   Future<List<UserModels>> getUsers();
 }
 
-const kCreateUserEndpoint = '/user';
-const kGetUsersEndpoint = '/user';
+const kCreateUserEndpoint = '/test-api/user';
+const kGetUsersEndpoint = '/test-api/user';
 
 class AuthRemoteDataSrcImpl implements AuthenticationRemoteDataSource {
   const AuthRemoteDataSrcImpl(this._client);
@@ -33,19 +35,48 @@ class AuthRemoteDataSrcImpl implements AuthenticationRemoteDataSource {
     // code is 200 or the proper response code
     // 2. check to make sure that it "THROWS A CUSTOM EXCEPTION" with the
     // right msg when status code is a bad one
-    final response = _client.post(
-      Uri.parse('$kBaseUrl$kCreateUserEndpoint'),
-      body: jsonEncode({
-        'createdAt': createdAt,
-        'name': name,
-        'avatar': avatar,
-      }),
-    );
+    try {
+      final response = await _client.post(
+        Uri.http(kBaseUrl, kCreateUserEndpoint),
+        body: jsonEncode({
+          'createdAt': createdAt,
+          'name': name,
+          'avatar': avatar,
+        }),
+      );
+
+      if (response.statusCode != 200 && response.statusCode != 201) {
+        throw APIException(
+          message: response.body,
+          statusCode: response.statusCode,
+        );
+      }
+    } on APIException {
+      rethrow;
+    } catch (e) {
+      throw APIException(message: e.toString(), statusCode: 505);
+    }
   }
 
   @override
   Future<List<UserModels>> getUsers() async {
-    // TODO: implement getUsers
-    throw UnimplementedError();
+    try {
+      final response = await _client.get(Uri.http(kBaseUrl, kGetUsersEndpoint));
+
+      if (response.statusCode != 200 && response.statusCode != 201) {
+        throw APIException(
+          message: response.body,
+          statusCode: response.statusCode,
+        );
+      }
+
+      return List<DataMap>.from(
+        jsonDecode(response.body) as List,
+      ).map((userData) => UserModels.fromMap(userData)).toList();
+    } on APIException {
+      rethrow;
+    } catch (e) {
+      throw APIException(message: e.toString(), statusCode: 505);
+    }
   }
 }
--- test/src/authentication/data/datasource/authentication_remote_data_source_test.dart
+++ test/src/authentication/data/datasource/authentication_remote_data_source_test.dart
@@ -3,8 +3,10 @@ import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:http/http.dart' as http;
+import 'package:testingapp/core/errors/exceptions.dart';
 import 'package:testingapp/core/utils/constants.dart';
 import 'package:testingapp/src/authentication/data/datasource/authentication_remote_data_source.dart';
+import 'package:testingapp/src/authentication/data/models/user_models.dart';
 
 class MockClient extends Mock implements http.Client {}
 
@@ -34,7 +36,7 @@ void main() {
         );
         verify(
           () => client.post(
-            Uri.parse('$kBaseUrl$kCreateUserEndpoint'),
+            Uri.http(kBaseUrl, kCreateUserEndpoint),
             body: jsonEncode({
               'createdAt': 'createdAt',
               'name': 'name',
@@ -46,10 +48,64 @@ void main() {
       },
     );
 
-    test(
-      'Should throw an [APIException] '
-      'when the status code is note 200 or 201',
-      () async {},
-    );
+    test('Should throw an [APIException] '
+        'when the status code is not 200 or 201', () async {
+      when(
+        () => client.post(any(), body: any(named: 'body')),
+      ).thenAnswer((_) async => http.Response('Invalid email address', 400));
+
+      final methodCall = remoteDataSrcImpl.createUser;
+
+      expect(
+        () =>
+            methodCall(createdAt: 'createdAt', name: 'name', avatar: 'avatar'),
+        throwsA(
+          APIException(message: 'Invalid email address', statusCode: 400),
+        ),
+      );
+      verify(
+        () => client.post(
+          Uri.http(kBaseUrl, kCreateUserEndpoint),
+          body: jsonEncode({
+            'createdAt': 'createdAt',
+            'name': 'name',
+            'avatar': 'avatar',
+          }),
+        ),
+      ).called(1);
+      verifyNoMoreInteractions(client);
+    });
+  });
+
+  group('getUsers', () {
+    final tUsers = [UserModels.empty()];
+    test('should complete successfully when'
+        'status code is 200 or 201', () async {
+      when(() => client.get(any())).thenAnswer(
+        (_) async => http.Response(jsonEncode([tUsers.first.toMap()]), 200),
+      );
+
+      final result = await remoteDataSrcImpl.getUsers();
+
+      expect(result, equals(tUsers));
+      verify(() => client.get(Uri.http(kBaseUrl, kGetUsersEndpoint))).called(1);
+      verifyNoMoreInteractions(client);
+    });
+
+    test('Should throw [APIException]'
+        'when status code is not 200 or 201', () async {
+      when(
+        () => client.get(any()),
+      ).thenAnswer((_) async => http.Response('Server down', 500));
+
+      final methodCall = remoteDataSrcImpl.getUsers;
+
+      expect(
+        () => methodCall(),
+        throwsA(const APIException(message: 'Server down', statusCode: 500)),
+      );
+      verify(() => client.get(Uri.http(kBaseUrl, kGetUsersEndpoint))).called(1);
+      verifyNoMoreInteractions(client);
+    });
   });
 }
