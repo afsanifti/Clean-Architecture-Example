17f331ec43a767a5129c32df0f6e6000c8c8c2a0
afsanifti <afsanisifti@gmail.com>
2025-05-25 21:43:46 +0600

    adding cubit

--- lib/src/authentication/presentation/cubit/authentication_cubit.dart
+++ lib/src/authentication/presentation/cubit/authentication_cubit.dart
@@ -1,8 +1,49 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../domain/entities/user.dart';
+import '../../domain/usecases/create_user.dart';
+import '../../domain/usecases/get_users.dart';
+
 part 'authentication_state.dart';
 
 class AuthenticationCubit extends Cubit<AuthenticationState> {
-  AuthenticationCubit() : super(AuthenticationInitial());
+  AuthenticationCubit({
+    required CreateUser createUser,
+    required GetUsers getUser,
+  }) : _createUser = createUser,
+       _getUsers = getUser,
+       super(const AuthenticationInitial());
+
+  final CreateUser _createUser;
+  final GetUsers _getUsers;
+
+  Future<void> createUser({
+    required String createdAt,
+    required String name,
+    required String avatar,
+  }) async {
+    emit(const CreatingUser());
+
+    final result = await _createUser(
+      CreateUserParams(createdAt: createdAt, name: name, avatar: avatar),
+    );
+
+    result.fold(
+      (failure) => emit(AuthenticationError(failure.errorMessage)),
+      (_) => emit(const UserCreated()),
+    );
+  }
+
+  Future<void> getUser() async {
+    emit(const GettingUsers());
+    final result = await _getUsers();
+
+    result.fold(
+      (failure) => emit(AuthenticationError(failure.errorMessage)),
+      // the GetUser call returns a List of User
+      // which is passed to the UsersLoaded
+      (user) => emit(UsersLoaded(user)),
+    );
+  }
 }
--- lib/src/authentication/presentation/cubit/authentication_state.dart
+++ lib/src/authentication/presentation/cubit/authentication_state.dart
@@ -2,9 +2,45 @@ part of 'authentication_cubit.dart';
 
 sealed class AuthenticationState extends Equatable {
   const AuthenticationState();
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
 }
 
 final class AuthenticationInitial extends AuthenticationState {
+  const AuthenticationInitial();
+}
+
+class CreatingUser extends AuthenticationState {
+  const CreatingUser();
+}
+
+class GettingUsers extends AuthenticationState {
+  const GettingUsers();
+}
+
+class UserCreated extends AuthenticationState {
+  const UserCreated();
+}
+
+class UsersLoaded extends AuthenticationState {
+  const UsersLoaded(this.users);
+
+  final List<User> users;
+
+  // Dart faces problem when checking if both lists are same.
+  // so we create a new List by mapping each user's id.
+  // we check if this list of user has the same ids as the next list of users
   @override
-  List<Object> get props => [];
+  List<Object?> get props => users.map((user) => user.id).toList();
+}
+
+class AuthenticationError extends AuthenticationState {
+  const AuthenticationError(this.message);
+
+  final String message;
+
+  @override
+  List<String?> get props => [message];
 }
